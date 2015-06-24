4d0867cdaac7e109a2da12ce052a6ca7a94c3931
Matt Basta <me@mattbasta.com>
2015-06-24 09:34:57 -0700

    Proxy requests directly to prevent bad analytics

--- src/modules/feeds.js
+++ src/modules/feeds.js
@@ -1,8 +1,15 @@
 var express = require('express');
+var superagent = require('superagent');
 
 var constants = require('../constants');
-var rss = require('../rss');
 
+
+var proxy_headers = [
+    'Accept-Charset',
+    'Accept-Language',
+    'Accept-Datetime',
+    'User-Agent',
+];
 
 var router = express.Router();
 
@@ -13,12 +20,26 @@ router.get('/rss/:feed', function(req, res) {
         return;
     }
 
-    rss.getRSS(constants.RSS_FEEDS[feedName], feedName, true).then(function(data) {
-        res.type('rss');
-        res.send(data);
-    }, function() {
-        res.status(500).end();
+    var url = constants.RSS_FEEDS[feedName];
+    var rssReq = superagent.get(url);
+    console.log('Requesting ' + url);
+
+    proxy_headers.forEach(function(h) {
+        var userH = req.get(h);
+        if (!userH) return;
+        rssReq = rssReq.set(h, userH);
     });
+
+    rssReq.buffer()
+        .end(function(err, resp) {
+            if (err) {
+                res.status(500).end();
+                return;
+            }
+
+            res.type('rss').send(resp.text);
+        });
+
 });
 
 
