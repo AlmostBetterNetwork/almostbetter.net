9a36c0e560f33b05a0843504de79a435b3e7a1b9
Matt Basta <me@mattbasta.com>
2017-01-22 07:36:54 -0800

    rip podbean

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,7 @@ exports.RSS_FEEDS = {
     'abtd': 'https://pinecast.com/feed/abtd',
     'ect': 'https://pinecast.com/feed/erie-canal-theatre',
     'lio': 'https://pinecast.com/feed/life-in-overdrive',
-    'otg': 'http://onetrackgamers.podbean.com/feed/',
+    'otg': 'https://pinecast.com/feed/one-track-gamers',
     'pcp': 'http://presscontinue.podbean.com/feed/',
     'tsk': 'https://pinecast.com/feed/tsk',
 };
