8f2fb3ed31954ff1d496accc7a38833b4d6d542d
Matt Basta <mattbasta+github@gmail.com>
2018-03-07 09:15:15 -0800

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -5,6 +5,6 @@ exports.RSS_FEEDS = {
     'ect': 'https://pinecast.com/feed/erie-canal-theatre',
     'lio': 'https://pinecast.com/feed/life-in-overdrive',
     'otg': 'https://pinecast.com/feed/one-track-gamers',
-    'pcp': 'http://presscontinue.podbean.com/feed/',
+    'pcp': 'https://pinecast.com/feed/press-continue-podcast',
     'tsk': 'https://pinecast.com/feed/tsk',
 };
